068154417382a975dc292c0140240d549b264ce3
Nick <nfenske@gmail.com>
2020-11-17 07:48:01 -0600

    Changes based off review

--- designs.js
+++ designs.js
@@ -1,22 +1,26 @@
+// Create the table
 function makeGrid() {
-    let gridHeight = document.getElementById('inputHeight').value;
-    let gridWidth = document.getElementById('inputWidth').value;
-    let table = document.getElementById('pixelCanvas');
-    let color = document.getElementById('colorPicker').value;
+    // Assign variables based off user input
+    const gridHeight = document.getElementById('inputHeight').value;
+    const gridWidth = document.getElementById('inputWidth').value;
+    const table = document.getElementById('pixelCanvas');
 
+    // Clear table each time this function is run
     table.innerHTML = '';
 
+    // Loop through input, creating rows, cells, and adding the event listener to change color for each cell
     for(let i=0; i < gridHeight; i++){
-        let newRow = table.insertRow(i);
+        const newRow = table.insertRow(i);
         for(let x=0; x < gridWidth; x++){
-            let newCell = newRow.insertCell(x);
+            const newCell = newRow.insertCell(x);
             newCell.addEventListener('click', function(e){
-                e.target.style.backgroundColor = color;
+                e.target.style.backgroundColor = document.getElementById('colorPicker').value;
             })
         }
     }
 }
 
+// Add event listener to the submit button to run the makeGrid function
 document.getElementById('sizePicker').addEventListener('submit', function(e){
     e.preventDefault();
     makeGrid();
